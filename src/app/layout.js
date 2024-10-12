3f3229f6424b71851e87dc43f348a381e2b437fd
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-10-12 07:52:17 +0530

    dark-mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import { ThemeProvider } from "next-themes"
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -9,13 +10,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
-        <main className="mt-8">
-        {children}
-        </main>
-        <Footer></Footer>
-        <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
-        </body>
+        <ThemeProvider attribute="class">
+          <Navbar></Navbar>
+          <main className="mt-8">
+            {children}
+          </main>
+          <Footer></Footer>
+          <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+          <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+          <script src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }

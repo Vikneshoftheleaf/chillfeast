830524d1672429e2dc521f5b974cc3150387a427
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2025-01-28 08:29:55 +0530

    blog

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -3,24 +3,25 @@ import fs from 'fs';
 import path from 'path';
 
 
-export default async function BlogPage({params})
+export const metadata ={
+    title:"Blog"
+}
 
-{
+export default async function BlogPage({ params }) {
     const slug = await params.slug
     const filePath = path.join(process.cwd(), 'src/data/blog.json');
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const data = JSON.parse(fileContent);
     const blog = data.find((item) => item.id === slug);
 
- 
-    return <BlogTemplate data={blog}/>
-   
-
-
+    return (<>
     
-}
+        <BlogTemplate data={blog} />
 
-export const metadata = {
-    title:  "Blog | Chillfeast"
-    
+    </>)
+
+
+
+
+
 }

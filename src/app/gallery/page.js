97b590865f94f1740441dd4bc28d1001465f197f
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2025-02-06 19:18:04 +0530

    gallery

---
+++ src/app/gallery/page.js
@@ -0,0 +1,11 @@
+import GalleryComponent from "@/components/gallery-component"
+export const metadata = {
+    "title": "Galley | Chillfeast",
+    "desciption": "Look at the works we done for ou clients"
+}
+export default function Gallery()
+{
+    return(
+       <GalleryComponent />
+    )
+}
---
+++ src/components/gallery-component.js
@@ -0,0 +1,247 @@
+import Image from "next/image"
+
+export default function GalleryComponent() {
+    return (
+
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (52).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (53).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (54).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (55).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (56).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (57).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (58).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (59).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (60).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (61).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (62).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (63).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (64).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (66).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (69).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (70).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (71).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (72).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (73).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (74).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (75).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (79).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (80).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (81).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (82).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (83).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (87).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (88).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (89).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (90).png' alt="" />
+                </div>
+            </div>
+
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (91).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (92).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (93).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (94).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (95).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (96).png' alt="" />
+                </div>
+            </div>
+
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (97).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (98).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (99).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (100).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (101).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (30).png' alt="" />
+                </div>
+            </div>
+
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (31).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (32).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (33).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (34).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (35).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (36).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (37).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (38).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (39).png' alt="" />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (40).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (41).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (42).png' alt="" />
+                </div>
+            </div>
+
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (70).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (44).png' alt="" />
+                </div>
+                
+            </div>
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (46).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (47).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (48).png' alt="" />
+                </div>
+            </div>
+
+            <div class="grid gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (49).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (50).png' alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src='/shots/Screenshot (51).png' alt="" />
+                </div>
+            </div>
+            
+            
+        </div>
+
+    )
+}
